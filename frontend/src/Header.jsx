fd1754d9ef1e2ad081304958cf375e0ae0adb79d
Davit <>
2025-11-28 12:41:08 +0400

    frontend with vite

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-400 text-white flex items-center justify-center h-16">
+      <p className="text-2xl font-semibold">ToDo List Online 🗒️</p>
+    </header>
+  );
+}
